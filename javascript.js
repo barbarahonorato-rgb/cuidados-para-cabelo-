f0603440a116ad90a9219dd0d2e40cd4aaf07b90
barbarahonorato-rgb <barbara.honorato@aluno.educa.go.gov.br>
2025-11-17 12:54:28 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,4 +5,15 @@ const dica =
 
 const dicas = [
   "evite lavar o cabelo com agua muito quente!"
-"corte as pontas a cada 2 meses para evitar pontas duplas
+"corte as pontas a cada 2 meses para evitar pontas duplas"
+" faça hidratação semenal com produtos naturais.",
+  " use protetot térmico antes de secar ou alisar.",
+  " durma com fronha de cetim para evitar o frizz.",
+  " massageie o couro cabeludo para estimular o crescimento.",
+  " não durma com o cabelo molhado!"];
+
+btnDica.addEventLister("click",()=>{
+  const dicaAleatoria =
+    dicas[math.random()*
+    dicas.length)];
+dica.textcontent = dicaAleatoria;});
